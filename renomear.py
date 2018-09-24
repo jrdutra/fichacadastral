8603a1a77a5f80adc8b874c18ad04fe987a794d9
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-09-24 20:39:15 -0300

    er

--- renomear.py
+++ renomear.py
@@ -9,6 +9,7 @@ from PIL import ImageDraw
 
 os.system("cls");
 pattern = "*.jpg"
+nome = input("NOME DA SEQUENCIA>>>")
 
 # Lê diretorio
 arquivo = open('dir.txt', 'r')
@@ -19,23 +20,14 @@ arquivo.close();
 # ENTRA NO DIRETORIO E PROCURA POR ARQUIVOS JPG
 qt_lida = 0
 porcento = 0
+i = 0
 for path, subdirs, files in os.walk(root):
     for name in files:
         if fnmatch(name, pattern):  # se nome do arquivo do subdir tiver a extensao
             dir = os.path.join(path, name)
             if os.path.isfile(dir+".txt") == 0:  # se nao existir o .txt com texto, cria
-                comandoAbrir = "start " + dir
-                comandoFechar = "taskkill.exe /IM Microsoft.Photos.exe /F"
-                os.system(comandoFechar)
-                os.system(comandoAbrir)
-                print(dir)
-                nome = input("NOME>>>")
-                img = Image.open(dir)
-                draw = ImageDraw.Draw(img)
-                texto = nome
-                font = ImageFont.truetype("arial-bold.ttf", 36)
-                draw.text((50, 50), nome, (0, 0, 0), font=font)
-                img.save(dir)
-                os.system("PAUSE");
-                os.system("cls");
+                i += 1
+                comando = "ren \"" + dir + "\" " + str(nome) + str(i) + ".jpg"
+                os.system(comando)
+                print(comando)
 
