06621b23f0cb9c70e595251c6bca4122999de7eb
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-09-24 13:03:16 -0300

    sdgsdg

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import pytesseract as ocr
 from PIL import Image
 
 
+os.system("cls");
 pattern = "*.jpg"
 
 # Lê diretorio
 arquivo = open('dir.txt', 'r')
 root = arquivo.read()
 arquivo.close();
+
 
 # ENTRA NO DIRETORIO E PROCURA POR ARQUIVOS JPG
 qt_lida = 0
@@ -20,5 +22,11 @@ for path, subdirs, files in os.walk(root):
         if fnmatch(name, pattern):  # se nome do arquivo do subdir tiver a extensao
             dir = os.path.join(path, name)
             if os.path.isfile(dir+".txt") == 0:  # se nao existir o .txt com texto, cria
+                comandoAbrir = "start " + dir
+                comandoFechar = "taskkill.exe /IM Microsoft.Photos.exe /F"
+                os.system(comandoFechar)
+                os.system(comandoAbrir)
                 print(dir)
+                os.system("PAUSE");
+                os.system("cls");
 
