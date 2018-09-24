6e45f3d1173d12049c6afca6918e2e4e7d29bf2d
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-09-24 12:52:37 -0300

    ewr

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from fnmatch import fnmatch
+import os.path
+import pytesseract as ocr
+from PIL import Image
+
+
+pattern = "*.jpg"
+
+# Lê diretorio
+arquivo = open('dir.txt', 'r')
+root = arquivo.read()
+arquivo.close();
+
+# ENTRA NO DIRETORIO E PROCURA POR ARQUIVOS JPG
+qt_lida = 0
+porcento = 0
+for path, subdirs, files in os.walk(root):
+    for name in files:
+        if fnmatch(name, pattern):  # se nome do arquivo do subdir tiver a extensao
+            dir = os.path.join(path, name)
+            if os.path.isfile(dir+".txt") == 0:  # se nao existir o .txt com texto, cria
+                print(dir)
+
