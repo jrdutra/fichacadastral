cb100e1c1593b85537be11ef35493e3e8ce8561c
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-09-24 21:29:28 -0300

    FUncionando

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ import pytesseract as ocr
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+from pywinauto import Application
 
+import win32com.client
+import win32gui
+import win32process
+import time
+
+import win32gui
+import re
 
 os.system("cls");
 pattern = "*.jpg"
@@ -19,16 +27,20 @@ arquivo.close();
 # ENTRA NO DIRETORIO E PROCURA POR ARQUIVOS JPG
 qt_lida = 0
 porcento = 0
+nomeprocesso = os.getpid()
 for path, subdirs, files in os.walk(root):
     for name in files:
         if fnmatch(name, pattern):  # se nome do arquivo do subdir tiver a extensao
             dir = os.path.join(path, name)
             if os.path.isfile(dir+".txt") == 0:  # se nao existir o .txt com texto, cria
+                os.system("cls")
                 comandoAbrir = "start " + dir
                 comandoFechar = "taskkill.exe /IM Microsoft.Photos.exe /F"
                 os.system(comandoFechar)
                 os.system(comandoAbrir)
                 print(dir)
+                app = Application().connect(process=int(nomeprocesso))
+                app.top_window().set_focus()
                 nome = input("NOME>>>")
                 img = Image.open(dir)
                 draw = ImageDraw.Draw(img)
@@ -36,6 +48,6 @@ for path, subdirs, files in os.walk(root):
                 font = ImageFont.truetype("arial-bold.ttf", 36)
                 draw.text((50, 50), nome, (0, 0, 0), font=font)
                 img.save(dir)
-                os.system("PAUSE");
-                os.system("cls");
+
+
 
